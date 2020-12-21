2d605ddcfe2b1fe83bf9f2907c4b284bae913c7a
moaabb <moab414@gmail.com>
2020-12-21 09:55:38 -0300

    added product details screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,21 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import { Container } from 'react-bootstrap'
 import HomeScreen from './Screens/HomeScreen'
- 
+import {BrowserRouter as Router, Route } from 'react-router-dom' 
+import ProductScreen from './Screens/ProductScreen'
+
 const App = () => {
   return (
-    <>
-    <Header/>
-    <main className='py-3'>
-      <Container>
-        <h1>Welcome to ProShop</h1>
-        <HomeScreen/>
-      </Container>
-    </main>
-    <Footer/>
-    </>
+    <Router>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen}/>
+        </Container>
+      </main>
+      <Footer/> 
+    </Router>
   )
 }
 
